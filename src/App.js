54a51cbea220bb935397f4014544aa3e80755a81
goanpixie <priyanka3007@gmail.com>
2020-12-07 12:08:33 -0800

    carousel init' ''
    '

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+let styles = {
+  margin: 'auto',
+  width: '500px'
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles}>
+      <Carousel>
+        <div>
+          <img
+            src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg"
+            alt="Hong Kong"
+          />
+          <p className="legend">Hong Kong</p>
+        </div>
+        <div>
+          <img
+            src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp"
+            alt="Singapore"
+          />
+          <p className="legend"></p>
+        </div>
+        <div>
+          <img
+            src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp"
+            alt="Japan"
+          />
+          <p className="legend">Japan</p>
+        </div>
+        <div>
+          <img
+            src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp"
+            alt="Las Vegas"
+          />
+          <p className="legend">Las Vegas</p>
+        </div>
+      </Carousel>
     </div>
   );
 }
-
 export default App;
